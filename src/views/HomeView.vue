fbd72a04aa0a2a51e633e59474ef95a048e348dc
rikanga <richardkangamba3@gmail.com>
2023-10-23 05:02:34 +0100

    initialization

---
+++ src/views/HomeView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="home">
+    <h1>Home Page</h1>
+    <p class='amber lighten-3  black--text'>Lorem ipesum dsioifoihf eijfoief eifiehjifhe eijfieohfe ifefoieoifeihfo eihfoiheoifhoiehfn
+        ezpojfez eojfpoz nefojepozjf eojfpozjfpozjf ezigoiezheznviejzoize ezijfpoejgezg opejozeg ojevzvnijgf opejozegoez
+        ezeg eojgez eojzeg ojpozegz izjgz eopojez oejigezceo ojecpoce cejpojigeg opjgzegnok poezjihgez iejpzeg pojegzeg
+        eopjgpozj ojgpoez eojpoejge ejojepzgepo ojezgz ijeoizg jpoiezg eeeeeeenezgez inoieg eijoije ezjpoe oijoieg eijig
+        jopie goizefezgi igeznijnzeni izee,kzqi igznncezjoez ezojp,c ,ni,c oezj cijezin,cnezjpoez eojpo, oijezn ejzgzen
+    </p>
+
+    <p class='pink lighten-4 red--text text--darken-3'>Lorem ipesum dsioifoihf eijfoief eifiehjifhe eijfieohfe ifefoieoifeihfo eihfoiheoifhoiehfn
+        ezpojfez eojfpoz nefojepozjf eojfpozjfpozjf ezigoiezheznviejzoize ezijfpoejgezg opejozeg ojevzvnijgf opejozegoez
+        ezeg eojgez eojzeg ojpozegz izjgz eopojez oejigezceo ojecpoce cejpojigeg opjgzegnok poezjihgez iejpzeg pojegzeg
+        eopjgpozj ojgpoez eojpoejge ejojepzgepo ojezgz ijeoizg jpoiezg eeeeeeenezgez inoieg eijoije ezjpoe oijoieg eijig
+        jopie goizefezgi igeznijnzeni izee,kzqi igznncezjoez ezojp,c ,ni,c oezj cijezin,cnezjpoez eojpo, oijezn ejzgzen
+    </p>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    // HelloWorld
+  }
+}
+</script>
